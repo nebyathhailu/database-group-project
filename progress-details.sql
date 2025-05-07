0066d9e686f9e65120f351b10e4533099658d10b
Rigbe Weleslasie <rigbewweleslasie@gmail.com>
2025-05-07 21:34:54 +0300

    Updates file names

--- progress-details.sql
+++ progress-details.sql
@@ -29,18 +29,22 @@ values('020','hewan','02-03-2020','female'),
 ('040','sami','17-05-2022','male');
 
 select * from malnutrition.child_data;
+
+
 CREATE TABLE  malnutrition.monthly_progress_details(
 progress_id VARCHAR(10) NOT NULL,
 child_id VARCHAR(10) NOT NULL,
 current_weight_in_kg DECIMAL NOT NULL,
 current_height_in_cm DECIMAL NOT NULL,
 CONSTRAINT progress_id PRIMARY KEY(progress_id),
-CONSTRAINT child_fk FOREIGN KEY(child_id)REFERENCES malnutrition.child_data(child_id),
-);
+CONSTRAINT child_fk FOREIGN KEY(child_id)REFERENCES malnutrition.child_data(child_id)
+)
+
+drop table malnutrition.monthly_progress_details;
 
 INSERT INTO malnutrition.monthly_progress_details(progress_id,child_id,current_weight_in_kg,current_height_in_cm)
 VALUES ('mpo1','020',7.5,64),
-('mpo2','021',8.0,62),database-group-project
+('mpo2','021',8.0,62),
 ('mpo3','022',9.0,63),
 ('mpo4','023',8.9,65),
 ('mpo5','024',8.5,62),
@@ -65,15 +69,5 @@ max(current_weight/power(height_in_cm/100.0,2)) as max_bmi,
 from malnutrion.monthly_progress_details, group by child_id as bmi compire so where max_bmi >min_bmi,
 
 );
-
-
-
-
-
-
-
-
-
-
 
 
