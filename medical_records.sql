5d87647f4ab6c625347402232ba93a6ed97dba67
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-05-07 19:22:12 +0300

    created and updated medical records table

--- medical_records.sql
+++ medical_records.sql
@@ -16,19 +16,19 @@ CREATE TABLE malnutrition.medical_records (
 
 INSERT INTO malnutrition.medical_records (record_id,child_id, visit_date, weight, height, treatment) VALUES
 ('R1','020', '2024-03-10', 14.5, 98.2, 'moderate malnutrition'),
-('R2','021', '2024-01-15', 16.0, 102.3, 'severe malnutrition'),
+('R2','021', '2024-01-15', 16.0, 92.3, 'severe malnutrition'),
 ('R3','022', '2024-05-20', 12.8, 95.0, 'mild malnutrition'),
 ('R4','024', '2024-06-10', 14.5, 98.2, 'moderate malnutrition'),
-('R5','025', '2024-07-15', 16.0, 102.3, 'severe malnutrition'),
+('R5','025', '2024-07-15', 16.0, 91.3, 'severe malnutrition'),
 ('R6','026', '2024-08-20', 12.8, 95.0, 'mild malnutrition'),
 ('R7','027', '2024-09-10', 14.5, 98.2, 'moderate malnutrition'),
 ('R8','028', '2024-01-15', 16.0, 102.3, 'severe malnutrition'),
 ('R10','029', '2024-11-20', 12.8, 95.0, 'mild malnutrition'),
 ('R11','030', '2024-12-10', 14.5, 98.2, 'moderate malnutrition'),
-('R12','031', '2024-8-15', 16.0, 102.3, 'severe malnutrition'),
+('R12','031', '2024-8-15', 16.0, 80.3, 'severe malnutrition'),
 ('R13','032', '2024-9-20', 12.8, 95.0, 'mild malnutrition'),
 ('R14','033', '2024-10-10', 14.5, 98.2, 'moderate malnutrition'),
-('R15','034', '2024-04-15', 16.0, 102.3, 'severe malnutrition');
+('R15','034', '2024-04-15', 16.0, 89.3, 'severe malnutrition');
 
 
 select * from malnutrition.medical_records;
